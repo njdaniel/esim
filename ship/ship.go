1ab93bedc14b6c925c45304a54f1a6aa44d9c9d5
nick <nicholasjdaniel@gmail.com>
2019-04-16 23:11:58 -0400

    add objects

---
+++ ship/ship.go
@@ -0,0 +1,54 @@
+package ship
+
+// Ship contains the data for a space ship
+type Ship struct {
+	Name           string
+	Shield         Defense
+	Armor          Defense
+	Hull           Defense
+	MaxTargetRange int
+	Capacitor      Capacitor
+	Weapons        []Weapon
+	MaxVelocity    int
+	CurVelocity    int
+	Inertia        float64
+	Mass           int
+	Radius         int
+}
+
+// Defense contains the hitpoints and the resistance
+type Defense struct {
+	MaxHP    int
+	CurHP    int
+	EMRes    float64
+	ExpRes   float64
+	KinRes   float64
+	TherRes  float64
+	Recharge int
+}
+
+// Capacitor is the power source to run the modules such as weapons and reppers. Has the max amount and the recharge rate.
+type Capacitor struct {
+	Capacity int
+	Percent  float64
+	Recharge int
+}
+
+// Weapon is a module that deals damage in the form of EM, Explosive, Kinetic, and Thermal.
+type Weapon struct {
+	EMDmg    int
+	ExpDmg   int
+	KinDmg   int
+	TherDmg  int
+	Rof      float64
+	Optimal  int
+	Falloff  int
+	Tracking float64
+}
+
+// ShieldBooster activated module to recover shield up to max.
+type ShieldBooster struct {
+	ShieldBonus    int
+	ActivationTime float64
+	ActivationCost int
+}
